152fb251c93ccf24a97b98c4464c1a8176fa58c0
Kaiyu Zheng <kaiyutony@gmail.com>
2022-01-09 16:46:51 -0500

    realsense not needed

--- setup_movo.bash
+++ setup_movo.bash
@@ -72,18 +72,6 @@ function install_libfreenect2
     cd $repo_root
 }
 
-function install_librealsense
-{
-    # Follow instructions here: https://github.com/IntelRealSense/librealsense/blob/master/doc/distribution_linux.md
-    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
-    sudo add-apt-repository "deb https://librealsense.intel.com/Debian/apt-repo $(lsb_release -cs) main" -u
-    sudo apt-get install librealsense2-dkms
-    sudo apt-get install librealsense2-utils
-    sudo apt-get install librealsense2-dev
-    sudo apt-get install librealsense2-dbg
-}
-
-
 ##------------------- Main Setup Logic ------------------ ##
 # use ros
 if ! useros; then
@@ -127,7 +115,6 @@ if first_time_build; then
         exit 1
     fi
     install_libfreenect2
-    install_librealsense
     sudo apt-get install libopencv-core-dev
 fi
 
