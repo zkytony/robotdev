ffe3afa120b385f72673db34f5ff584045389b9d
Kaiyu Zheng <kaiyutony@gmail.com>
2022-01-16 23:26:23 -0500

    put config in skill file

--- shared/src/framework.cpp
+++ shared/src/framework.cpp
@@ -2,6 +2,7 @@
 #include <yaml-cpp/yaml.h>
 #include <iostream>
 #include <assert.h>
+#include <stdexcept>
 
 namespace rbd
 {
@@ -13,10 +14,26 @@ SkillManager::SkillManager() {
 void SkillManager::Load(const std::string &skill_file_path) {
 
     YAML::Node skill_spec = YAML::LoadFile(skill_file_path);
-    for (YAML::const_iterator it=skill_spec.begin(); it != skill_spec.end(); ++it) {
-        YAML::Node checkpoint = *it;
-        std::cout << checkpoint.Type() << std::endl;  // 4 means Map
+
+    // First, create a map from cue type to verifier and executor types
+    if (!skill_spec["config"] || !skill_spec["skill"]) {
+        throw std::runtime_error("Expects both 'config' and 'skill' in the skill file.");
     }
+
+
+
+    // for (YAML::const_iterator it=skill_spec.begin(); it != skill_spec.end(); ++it) {
+    //     YAML::Node checkpoint_spec = *it;
+
+    //     if (checkpoint_spec["perception_cues"]) {
+
+    //     }
+
+    //     checkpoint_spec["name"];
+
+
+    //     std::cout << checkpoint.Type() << std::endl;  // 4 means Map
+    // }
 
 }
 
--- shared/src/hello_world.cpp
+++ shared/src/hello_world.cpp
@@ -6,7 +6,7 @@
 // rosrun rbd_movo_motor_skills hello_world src/rbd_movo_action/rbd_movo_motor_skills/cfg/skills/general.left_clearaway.skill
 int main(int argc, char **argv) {
     rbd::SkillManager mgr;
-    std::cout << "Hello world!";
+    std::cout << "Hello world!"; << std::endl;
     std::cout << "Attempting to open " << std::string(argv[1]) << std::endl;
     mgr.Load(std::string(argv[1]));
 }
