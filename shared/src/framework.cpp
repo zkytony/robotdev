2e3d9e599eb1abb6e1adb8e7dde60bec01422b8d
Kaiyu Zheng <kaiyutony@gmail.com>
2022-01-16 23:15:35 -0500

    able to load yaml file (not parsed yet)

--- shared/src/framework.cpp
+++ shared/src/framework.cpp
@@ -1,4 +1,7 @@
 #include "motion_planning/framework.h"
+#include <yaml-cpp/yaml.h>
+#include <iostream>
+#include <assert.h>
 
 namespace rbd
 {
@@ -8,6 +11,13 @@ SkillManager::SkillManager() {
 }
 
 void SkillManager::Load(const std::string &skill_file_path) {
+
+    YAML::Node skill_spec = YAML::LoadFile(skill_file_path);
+    for (YAML::const_iterator it=skill_spec.begin(); it != skill_spec.end(); ++it) {
+        YAML::Node checkpoint = *it;
+        std::cout << checkpoint.Type() << std::endl;  // 4 means Map
+    }
+
 }
 
 
--- shared/src/hello_world.cpp
+++ shared/src/hello_world.cpp
@@ -1,7 +1,12 @@
 #include "motion_planning/framework.h"
 #include <iostream>
+#include <string>
 
+// to test this program run (from robotdev/movo):
+// rosrun rbd_movo_motor_skills hello_world src/rbd_movo_action/rbd_movo_motor_skills/cfg/skills/general.left_clearaway.skill
 int main(int argc, char **argv) {
     rbd::SkillManager mgr;
     std::cout << "Hello world!";
+    std::cout << "Attempting to open " << std::string(argv[1]) << std::endl;
+    mgr.Load(std::string(argv[1]));
 }
