e4a11ca0339acc128c2bc13e80241f559d20819b
Kaiyu Zheng <kaiyutony@gmail.com>
2022-01-25 10:30:56 -0500

    more tests

---
+++ movo/src/rbd_movo_action/rbd_movo_motor_skills/tests/test_framework_arm_reset.py
@@ -0,0 +1,13 @@
+import rospy
+from rbd_movo_motor_skills.motion_planning.framework import SkillManager
+
+def test():
+    rospy.set_param("skill/pkg_base_dir", "../")
+    rospy.set_param("skill/pkg_name", "rbd_movo_motor_skills")
+    skill_file_path = "general.left_arm.reset.skill"
+    mgr = SkillManager(skill_file_path)
+    mgr.run()
+
+
+if __name__ == "__main__":
+    test()
