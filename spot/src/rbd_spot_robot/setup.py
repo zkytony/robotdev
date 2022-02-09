946e74b4d348b7b3036b8bde54a249e0d90c62c7
Kaiyu Zheng <kaiyutony@gmail.com>
2022-02-08 20:28:57 -0500

    ditto (setup.py)

---
+++ spot/src/rbd_spot_robot/setup.py
@@ -0,0 +1,14 @@
+## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+
+from distutils.core import setup
+from setuptools import find_packages
+from catkin_pkg.python_setup import generate_distutils_setup
+
+# fetch values from package.xml
+setup_args = generate_distutils_setup(
+    packages=['rbd_spot_robot'],
+    package_dir={'': 'src'},
+    requires=['std_msgs', 'rospy', 'tf', 'roscpp', 'nav_msgs']
+)
+
+setup(**setup_args)
