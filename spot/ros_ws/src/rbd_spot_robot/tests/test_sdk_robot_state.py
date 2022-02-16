c40d93fc18d4d4c99d0c6cc2e312a3a36783c048
Kaiyu Zheng <kaiyutony@gmail.com>
2022-02-16 17:59:56 -0500

    test frequency for robot state service call

--- spot/ros_ws/src/rbd_spot_robot/tests/test_sdk_robot_state.py
+++ spot/ros_ws/src/rbd_spot_robot/tests/test_sdk_robot_state.py
@@ -1,8 +1,18 @@
 from rbd_spot_perception.robot_state import RobotStateClient
+import time
+from tqdm import tqdm
 
 def test():
     c = RobotStateClient()
-    print(c.get())
+    start_time = time.time()
+    total_num = 100
+    for i in tqdm(range(total_num)):
+        c.get()
+    time_used = time.time() - start_time
+    print(f"Frequency (tested with {total_num} calls): {total_num / time_used}")
 
 if __name__ == "__main__":
     test()
+    # output I got:
+    # 100%|████████████████████████████████| 100/100 [00:01<00:00, 68.87it/s]
+    # Frequency (tested with 100 calls): 68.01107167237708
