a85c3bc0645d6850102d59956506efcd46646837
Kaiyu Zheng <kaiyutony@gmail.com>
2022-02-21 14:52:47 -0500

    can get image response

--- spot/ros_ws/src/rbd_spot_perception/scripts/stream_image.py
+++ spot/ros_ws/src/rbd_spot_perception/scripts/stream_image.py
@@ -10,7 +10,7 @@ import argparse
 
 from rbd_spot_robot import SpotSDKConn
 from bosdyn.api import image_pb2
-from bosdyn.client.image import ImageClient
+from bosdyn.client.image import ImageClient, build_image_request
 
 import pandas as pd
 from pprint import pprint
@@ -29,13 +29,17 @@ def extract_source_names(sources_result):
     """
     # obtain enum name from enum value
     _name_func = image_pb2.ImageSource.ImageType.Name
-    return pd.DataFrame([(source.name, _name_func(source.image_type))
-                         for source in sources_result],
-                        columns=["name", "type"])
+    return pd.DataFrame(sorted([(source.name, _name_func(source.image_type))
+                                for source in sources_result],
+                               key=lambda tup: tup[1]),  # sort by image type
+                               columns=["name", "type"])
 
 def main():
     parser = argparse.ArgumentParser("stream image")
     parser.add_argument("sources", nargs="+", help="image sources; or 'list'")
+    parser.add_argument("-q", "--quality", type=int, help="image quality [0-100]", default=75)
+    formats = ["UNKNOWN", "JPEG", "RAW", "RLE"]
+    parser.add_argument("-f", "--format", type=str, help="format", choices=formats)
     args = parser.parse_args()
 
     conn = SpotSDKConn(sdk_name="StreamImageClient")
@@ -50,6 +54,20 @@ def main():
         print("ListImageSources took %.3fs" % _used_time)
         df = extract_source_names(sources_result)
         print(df)
+    else:
+        # Stream the image through specified sources
+        requests = []
+        for source in sources:
+            fmt = None
+            if args.format:
+                fmt = image_pb2.Image.Format.Value(formats.index(args.format))
+            req = build_image_request(source, quality_percent=args.quality, image_format=fmt)
+            requests.append(req)
+        _start_time = time.time()
+        response = image_client.get_image(requests)
+        _used_time = time.time() - _start_time
+        print("GetImage took %.3fs" % _used_time)
+
 
 if __name__ == "__main__":
     main()
