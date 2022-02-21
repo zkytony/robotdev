1db0a7b97c210188d9f1d9685198da297b8ad1d6
Kaiyu Zheng <kaiyutony@gmail.com>
2022-02-21 10:55:53 -0500

    finally coded the way should w/ sdk

--- spot/ros_ws/src/rbd_spot_perception/scripts/stream_image.py
+++ spot/ros_ws/src/rbd_spot_perception/scripts/stream_image.py
@@ -5,9 +5,14 @@
 #
 # rosrun rbd_spot_perception stream_image.py list
 
+import time
 import argparse
+
 from rbd_spot_robot import SpotSDKConn
-import bosdyn.client as bdc
+from bosdyn.api import image_pb2
+from bosdyn.client.image import ImageClient
+
+import pandas as pd
 from pprint import pprint
 
 
@@ -17,9 +22,16 @@ def image_callback():
 def extract_source_names(sources_result):
     """
     Args:
-        sources_result (list of ListImageSources)
+        sources_result: response of ListImageSources service.
+            it is a set of ImageSource data objects
+    Returns:
+        a DataFrame object with two columns: name, and type
     """
-
+    # obtain enum name from enum value
+    _name_func = image_pb2.ImageSource.ImageType.Name
+    return pd.DataFrame([(source.name, _name_func(source.image_type))
+                         for source in sources_result],
+                        columns=["name", "type"])
 
 def main():
     parser = argparse.ArgumentParser("stream image")
@@ -27,13 +39,17 @@ def main():
     args = parser.parse_args()
 
     conn = SpotSDKConn(sdk_name="StreamImageClient")
-    image_client = conn.ensure_client(bdc.image.ImageClient.default_service_name)
+    image_client = conn.ensure_client(ImageClient.default_service_name)
 
     sources = args.sources
     if len(sources) == 1 and sources[0] == "list":
         # list sources; blocking call
-        print(image_client.list_image_sources())
-
+        _start_time = time.time()
+        sources_result = image_client.list_image_sources()
+        _used_time = time.time() - _start_time
+        print("ListImageSources took %.3fs" % _used_time)
+        df = extract_source_names(sources_result)
+        print(df)
 
 if __name__ == "__main__":
     main()
