54a5862940b5ec129c0df69f0c1dcdf27e4b7f96
Kaiyu Zheng <kaiyutony@gmail.com>
2022-04-27 22:38:22 -0400

    add bashscript to get python 3.9

---
+++ spot/viam/get_python3.9.sh
@@ -0,0 +1,17 @@
+## Download and install Python
+PYTHON_VERSION="3.9.12"
+if [ ! -d "$HOME/software/Python-$PYTHON_VERSION" ]
+then
+    mkdir ~/software
+    cd ~/software/
+    echo -e "Installing Python ${PYTHON_VERSION}"
+    if [ ! -f "Python-${PYTHON_VERSION}.tgz" ]
+    then
+        wget "https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tgz"
+    fi
+    tar xzvf "Python-${PYTHON_VERSION}.tgz"
+    cd "Python-${PYTHON_VERSION}"
+    ./configure
+    make
+    make test
+fi
