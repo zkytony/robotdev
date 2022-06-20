60b8f10650496cc769e2b3345dde1df133562ae2
Kaiyu Zheng <kaiyutony@gmail.com>
2022-06-19 20:06:41 -0400

    publishes PointCloud2 message

--- spot/ros_ws/src/rbd_spot_perception/include/rbd_spot_perception/graphnav_map_publisher.h
+++ spot/ros_ws/src/rbd_spot_perception/include/rbd_spot_perception/graphnav_map_publisher.h
@@ -2,25 +2,33 @@
 #define GRAPHNAV_MAP_PUBLISHER_H
 
 #include <string>
+#include <vector>
 #include <Python.h>
 #include <numpy/arrayobject.h>
 #include <pcl_ros/point_cloud.h>
 
 using std::string;
+using std::vector;
+
+typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 
 // Loads a GraphNav map as point cloud and
 // publishes the point cloud as a ROS message
 class GraphNavMapPublisher {
 public:
-    GraphNavMapPublisher(string map_path, string pub_topic);
-
+    GraphNavMapPublisher(string map_path);
+    void run();
 private:
+    ros::NodeHandle nh_;
+    ros::Publisher pcl_pub_;
+    string pcl_frame_id_;
+    double pcl_rate_;
     string map_path_;
     string pub_topic_;
-    pcl::PointCloud<pcl::PointXYZ> *point_cloud_;
+    PointCloud cloud_;
 
     void loadMap_();
-    void parse_points_array_(PyArrayObject *dataArray);
+    void parsePointsArray_(PyArrayObject *dataArray);
 };
 
 #endif
--- spot/ros_ws/src/rbd_spot_perception/src/rbd_spot_perception/graphnav_map_publisher.cpp
+++ spot/ros_ws/src/rbd_spot_perception/src/rbd_spot_perception/graphnav_map_publisher.cpp
@@ -1,20 +1,47 @@
 #define PY_SSIZE_T_CLEAN
 
+// standard libs
 #include <iostream>
 #include <vector>
+
+// python and numpy C APIs
 #include <Python.h>
 #include <numpy/arrayobject.h>
+
+// ros and PCL
+#include <ros/ros.h>
 #include <pcl_ros/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <sensor_msgs/PointCloud2.h>
+
+// our headers
 #include "graphnav_map_publisher.h"
 #include "utils/c_api_utils.h"
 
 using std::string;
 using std::vector;
 
-GraphNavMapPublisher::GraphNavMapPublisher(string map_path, string pub_topic)
-    : map_path_(map_path), pub_topic_(pub_topic) {
+GraphNavMapPublisher::GraphNavMapPublisher(string map_path)
+    : map_path_(map_path) {
+    loadMap_();
 
-    this->loadMap_();
+    pub_topic_ = "graphnav_map_points";
+    if (nh_.hasParam("topic")) {
+        nh_.getParam("topic", pub_topic_);
+    }
+
+    pcl_frame_id_ = "graphnav_map";
+    if (nh_.hasParam("frame_id")) {
+        nh_.getParam("frame_id", pcl_frame_id_);
+    }
+
+    pcl_rate_ = 4.0;
+    if (nh_.hasParam("rate")) {
+        nh_.getParam("rate", pcl_rate_);
+    }
+
+    pcl_pub_ = nh_.advertise<sensor_msgs::PointCloud2>(pub_topic_, 10);
 }
 
 
@@ -33,7 +60,7 @@ void GraphNavMapPublisher::loadMap_() {
     if (dataObj != NULL) {
         PyArrayObject *dataArray = obj_to_array_no_conversion(dataObj, NPY_DOUBLE);
         if (dataArray != NULL) {
-            this->parse_points_array_(dataArray);
+            this->parsePointsArray_(dataArray);
         } else {
             goto fail;
         }
@@ -49,27 +76,44 @@ void GraphNavMapPublisher::loadMap_() {
         fprintf(stderr,"Call failed\n");
 }
 
-void GraphNavMapPublisher::parse_points_array_(PyArrayObject *dataArray) {
+/* given the numpy data array returned by the load_map_as_points function,
+ * converts it into a vector<pcl::PointXYZ> object to be easier to work with. */
+void GraphNavMapPublisher::parsePointsArray_(PyArrayObject *dataArray) {
     auto size = PyArray_DIM(dataArray, 0);
-    std::cout << size << std::endl;
-
     pcl::PointXYZ* points_arr = (pcl::PointXYZ*) PyArray_DATA(dataArray);
-    vector<pcl::PointXYZ> point_cloud(points_arr, points_arr + size);
-    this->point_cloud_ = point_cloud;
-    for (pcl::PointXYZ point: this->point_cloud_) {
-        std::cout << "[" << point.x << ", " << point.y << ", " << point.z << "]" << std::endl;
+    for (int i=0; i<size; i++) {
+        this->cloud_.push_back(points_arr[i]);
+    }
+    std::cout << "Loaded " << size << " points." << std::endl;
+}
+
+void GraphNavMapPublisher::run() {
+
+    sensor_msgs::PointCloud2 pcl_msg;
+    pcl_msg.header.frame_id = this->pcl_frame_id_;
+    pcl::toROSMsg(this->cloud_, pcl_msg);
+    // // sanity check; height should be 1 and width should be size
+    // std::cout << pcl_msg.height << std::endl;
+    // std::cout << pcl_msg.width << std::endl;
+    ros::Rate loop_rate(this->pcl_rate_);
+    while (this->nh_.ok()) {
+        pcl_msg.header.stamp = ros::Time::now();
+        this->pcl_pub_.publish(pcl_msg);
+        ros::spinOnce();
+        loop_rate.sleep();
     }
 }
 
 
 int main(int argc, char *argv[]) {
-
     if (argc < 2) {
         std::cout << "Usage: " << argv[0] << " <path_to_map_directory>" << std::endl;
         return 1;
     }
     Py_Initialize();
     init_numpy();
-    GraphNavMapPublisher mapPub = GraphNavMapPublisher(argv[1], "map");
+    ros::init(argc, argv, "graphnav_map_publisher");
+    GraphNavMapPublisher mapPub = GraphNavMapPublisher(argv[1]);
+    mapPub.run();
     Py_Finalize();
 }
