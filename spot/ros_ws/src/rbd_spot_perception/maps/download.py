ec96d56a14f96434bac3980bdadac7b8f0265b2c
Kaiyu Zheng <kaiyutony@gmail.com>
2022-06-20 17:52:45 -0400

    update cit first floor map gdrive file

--- spot/ros_ws/src/rbd_spot_perception/maps/download.py
+++ spot/ros_ws/src/rbd_spot_perception/maps/download.py
@@ -6,7 +6,7 @@ outfname = "cit_first_floor.zip"
 output = f"bosdyn/{outfname}"
 if not os.path.exists(output):
     print("Downloading CIT First Floor GraphNav map")
-    foref_models_url = "https://drive.google.com/uc?id=14-qcpkUMBP_WN7PnXC1w7fqbZdLVzTEz"
+    foref_models_url = "https://drive.google.com/uc?id=1jO4tLMtC56Wi1M5xXJlGEmRRX6f-UW-I"
     gdown.download(foref_models_url, output, quiet=False)
     cmd=f'''
 cd bosdyn
