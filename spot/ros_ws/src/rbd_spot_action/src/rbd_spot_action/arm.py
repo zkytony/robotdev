ceec86a64b3747fcbb337b2ccb215a50fda8a002
Kaiyu Zheng <kaiyutony@gmail.com>
2022-07-03 15:04:41 -0400

    arm gaze at command (works expectedly)

--- spot/ros_ws/src/rbd_spot_action/src/rbd_spot_action/arm.py
+++ spot/ros_ws/src/rbd_spot_action/src/rbd_spot_action/arm.py
@@ -2,16 +2,44 @@
 import time
 from bosdyn.client.robot_command import (RobotCommandBuilder, RobotCommandClient,
                                          block_until_arm_arrives, blocking_stand)
+from bosdyn.client.frame_helpers import GRAV_ALIGNED_BODY_FRAME_NAME, ODOM_FRAME_NAME, get_a_tform_b
 
 def create_client(conn):
     return conn.ensure_client(RobotCommandClient.default_service_name)
 
-def unstow(command_client, conn):
+def unstow(conn, command_client):
     """unstow the arm. Blocks until the arm arrives"""
     unstow = RobotCommandBuilder.arm_ready_command()
+    return _execute_arm_command(unstow, command_client, conn.lease)
+
+def stow(conn, command_client):
+    """unstow the arm. Blocks until the arm arrives"""
+    stow = RobotCommandBuilder.arm_stow_command()
+    return _execute_arm_command(stow, command_client, conn.lease)
+
+
+def gazeAt(conn, command_client, x, y, z):
+    """Opens the gripper, and gaze at a target pose. The target pose
+    can be specified with respect to the robot's body. Reference:
+    Spot SDK arm_gaze example. WARNING: ARM MAY BEHAVE UNEXPECTEDLY"""
+    gaze_command = RobotCommandBuilder.arm_gaze_command(x, y, z,
+                                                        GRAV_ALIGNED_BODY_FRAME_NAME)
+    # Make the open gripper RobotCommand
+    gripper_command = RobotCommandBuilder.claw_gripper_open_command()
+
+    # Combine the arm and gripper commands into one RobotCommand
+    synchro_command = RobotCommandBuilder.build_synchro_command(
+        gripper_command, gaze_command)
+
+    # Send the request
+    print("Requesting gaze.")
+    return _execute_arm_command(synchro_command, command_client, conn.lease, wait=4.0)
+
+
+def _execute_arm_command(command, command_client, lease, wait=3.0):
     _start_time = time.time()
     # note that this gets the graph directly instead of a Response object
-    unstow_command_id = command_client.robot_command(unstow, lease=conn.lease.lease_proto)
-    block_until_arm_arrives(command_client, unstow_command_id, 3.0)
+    command_id = command_client.robot_command(command, lease=lease.lease_proto)
+    block_until_arm_arrives(command_client, command_id, wait)
     _used_time = time.time() - _start_time
-    return unstow_command_id, _used_time
+    return command_id, _used_time
--- spot/ros_ws/src/rbd_spot_action/tests/test_stow_unstow_arm.py
+++ spot/ros_ws/src/rbd_spot_action/tests/test_stow_unstow_arm.py
@@ -2,12 +2,15 @@
 # unstow the arm
 import time
 import rbd_spot
+from bosdyn.client.robot_command import blocking_stand
 
 def main():
     conn = rbd_spot.SpotSDKConn(sdk_name="UnstowArmClient",
                                 take_lease=True)
     command_client = rbd_spot.arm.create_client(conn)
-    rbd_spot.arm.unstow(command_client, conn)
+    rbd_spot.arm.unstow(conn, command_client)
+    time.sleep(3.0)
+    rbd_spot.arm.stow(conn, command_client)
     time.sleep(3.0)
 
 if __name__ == "__main__":
