7a8a06c1f025d757b907e84689d44ffed4f96834
Kaiyu Zheng <kaiyutony@gmail.com>
2022-07-04 12:46:26 -0400

    improving plotting

--- shared/python/utils/vision/plotting.py
+++ shared/python/utils/vision/plotting.py
@@ -28,24 +28,24 @@ def plot_one_box(img, xyxy, label, color, line_thickness=3, show_label=True):
 # Index of the class in the list is its ID. For example, to get ID of
 # the teddy bear class, use: class_names.index('teddy bear')
 # sourec: https://github.com/matterport/Mask_RCNN/blob/master/samples/demo.ipynb
-COCO_CLASS_NAMES = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
-                    'bus', 'train', 'truck', 'boat', 'traffic light',
-                    'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
-                    'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
-                    'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
-                    'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
-                    'kite', 'baseball bat', 'baseball glove', 'skateboard',
-                    'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
-                    'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
-                    'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
-                    'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
-                    'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
-                    'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
-                    'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
-                    'teddy bear', 'hair drier', 'toothbrush']
+COCO_CLASS_NAMES = [
+    '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+    'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
+    'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+    'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
+    'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+    'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+    'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+    'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+    'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
+    'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+    'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
+    'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+]
 
-
-def maskrcnn_draw_result(prediction, img, mask_threshold=0.5, class_names=COCO_CLASS_NAMES):
+def maskrcnn_draw_result(prediction, img,
+                         mask_threshold=0.5,
+                         class_names=COCO_CLASS_NAMES):
     """
     Let's say you do:
 
@@ -67,6 +67,9 @@ def maskrcnn_draw_result(prediction, img, mask_threshold=0.5, class_names=COCO_C
     result_img = torchvision.utils.draw_segmentation_masks(img, masks, alpha=0.6)
 
     boxes = prediction['boxes']
-    labels = [class_names[label_index] for label_index in prediction['labels']]
-    result_img = torchvision.utils.draw_bounding_boxes(result_img, boxes, labels, font_size=15)
+    scores = prediction['scores']
+    labels = ["{}({:.2f})".format(class_names[prediction['labels'][i]], scores[i])
+              for i in range(len(scores))]
+    result_img = torchvision.utils.draw_bounding_boxes(
+        result_img, boxes, labels, font_size=15)
     return result_img
