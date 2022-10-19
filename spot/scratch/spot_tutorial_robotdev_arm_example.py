2fb5728ca7e6086399ac7dcfd4fd771a39b2d37e
Kaiyu Zheng <kaiyutony@gmail.com>
2022-10-19 17:01:26 -0400

    arm example for tutorial

---
+++ spot/scratch/spot_tutorial_robotdev_arm_example.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# unstow the arm
+import time
+import rbd_spot
+from bosdyn.client.robot_command import blocking_stand
+
+def main():
+    conn = rbd_spot.SpotSDKConn(sdk_name="ArmMoveClient",
+                                take_lease=True)
+    command_client = rbd_spot.arm.create_client(conn)
+    robot_state_client = rbd_spot.state.create_client(conn)
+    blocking_stand(command_client, timeout_sec=10)
+    # NOTE: Don't try 0.0, 0.0, 0.0 ==> the robot will attampt
+    # to look at its own center of gravity (body frame origin)
+    # which is leads to unpleasant arm motion.
+
+    # +x: forward
+    # +y: left
+    # +z: up
+
+    # For reference:
+    # x = 0.5 (almost touching the robot itself)
+    # x = 1.0 (good number for testing)
+    # x = 1.5 (too far ahead)
+    print((1.0, 0.0, 0))
+    rbd_spot.arm.moveEETo(conn, command_client, robot_state_client, (0.75, 0.2, 0.4))
+    time.sleep(2.0)
+    rbd_spot.arm.stow(conn, command_client)
+    time.sleep(2.0)
+
+    # Test if the robot can move itself to reach a farther pose
+    # answer: No it can't
+    print((2.0, -0.5, 0.25))
+    rbd_spot.arm.moveEETo(conn, command_client, robot_state_client, (2.0, 0.1, 0.25))
+    time.sleep(2.0)
+    rbd_spot.arm.stow(conn, command_client)
+    time.sleep(2.0)
+
+    # Now, do the body follow command. It should achieve the goal
+    print((2.0, -0.5, 0.25))
+    rbd_spot.arm.moveEEToWithBodyFollow(conn, command_client, robot_state_client, (2.0, 0.1, 0.25))
+    time.sleep(2.0)
+    rbd_spot.arm.stow(conn, command_client)
+    time.sleep(2.0)
+
+if __name__ == "__main__":
+    main()
