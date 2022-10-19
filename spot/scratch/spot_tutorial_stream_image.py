c9eff7cde2874bc78e8be14343e8e1dffb183839
Kaiyu Zheng <kaiyutony@gmail.com>
2022-10-19 15:24:05 -0400

    make it stream

--- spot/scratch/spot_tutorial_stream_image.py
+++ spot/scratch/spot_tutorial_stream_image.py
@@ -31,39 +31,42 @@ def main():
     # Create Client & Call Service
     image_client = robot.ensure_client(ImageClient.default_service_name)
 
-    image_request = image_pb2.ImageRequest(image_source_name="frontleft_fisheye_image")
-    for image_response in image_client.get_image([image_request]):
-        print(image_response.source)
-        num_bytes = 1  # Assume a default of 1 byte encodings.
-        if image_response.shot.image.pixel_format == image_pb2.Image.PIXEL_FORMAT_DEPTH_U16:
-            dtype = np.uint16
-            extension = ".png"
-        else:
-            if image_response.shot.image.pixel_format == image_pb2.Image.PIXEL_FORMAT_RGB_U8:
-                num_bytes = 3
-            elif image_response.shot.image.pixel_format == image_pb2.Image.PIXEL_FORMAT_RGBA_U8:
-                num_bytes = 4
-            elif image_response.shot.image.pixel_format == image_pb2.Image.PIXEL_FORMAT_GREYSCALE_U8:
-                num_bytes = 1
-            elif image_response.shot.image.pixel_format == image_pb2.Image.PIXEL_FORMAT_GREYSCALE_U16:
-                num_bytes = 2
-            dtype = np.uint8
-            extension = ".jpg"
+    while True:
+        image_request = image_pb2.ImageRequest(image_source_name="frontleft_fisheye_image")
+        for image_response in image_client.get_image([image_request]):
+            print(image_response.source)
+            num_bytes = 1  # Assume a default of 1 byte encodings.
+            if image_response.shot.image.pixel_format == image_pb2.Image.PIXEL_FORMAT_DEPTH_U16:
+                dtype = np.uint16
+                extension = ".png"
+            else:
+                if image_response.shot.image.pixel_format == image_pb2.Image.PIXEL_FORMAT_RGB_U8:
+                    num_bytes = 3
+                elif image_response.shot.image.pixel_format == image_pb2.Image.PIXEL_FORMAT_RGBA_U8:
+                    num_bytes = 4
+                elif image_response.shot.image.pixel_format == image_pb2.Image.PIXEL_FORMAT_GREYSCALE_U8:
+                    num_bytes = 1
+                elif image_response.shot.image.pixel_format == image_pb2.Image.PIXEL_FORMAT_GREYSCALE_U16:
+                    num_bytes = 2
+                dtype = np.uint8
+                extension = ".jpg"
 
-        img = np.frombuffer(image_response.shot.image.data, dtype=dtype)
-        if image_response.shot.image.format == image_pb2.Image.FORMAT_RAW:
-            try:
-                # Attempt to reshape array into a RGB rows X cols shape.
-                img = img.reshape((image_response.shot.image.rows,
-                                   image_response.shot.image.cols, num_bytes))
-            except ValueError:
-                # Unable to reshape the image data, trying a regular decode.
+            img = np.frombuffer(image_response.shot.image.data, dtype=dtype)
+            if image_response.shot.image.format == image_pb2.Image.FORMAT_RAW:
+                try:
+                    # Attempt to reshape array into a RGB rows X cols shape.
+                    img = img.reshape((image_response.shot.image.rows,
+                                       image_response.shot.image.cols, num_bytes))
+                except ValueError:
+                    # Unable to reshape the image data, trying a regular decode.
+                    img = cv2.imdecode(img, -1)
+            else:
                 img = cv2.imdecode(img, -1)
-        else:
-            img = cv2.imdecode(img, -1)
 
-        plt.imshow(img, cmap='gray')
-        plt.show()
+            plt.imshow(img, cmap='gray')
+            plt.show(block=False)
+            plt.pause(1)
+
 
 if __name__ == "__main__":
     main()
