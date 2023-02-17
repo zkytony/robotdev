ba44034efb37f411f858a036ba8c87ebecce1439
Kaiyu Zheng <kzheng@theaiinstitute.com>
2023-02-17 18:06:11 -0500

    upgrade spot sdk from 3.1.0 to 3.2.1 & tested img streaming & arm mv

--- setup_spot.bash
+++ setup_spot.bash
@@ -141,11 +141,11 @@ if first_time_build $SPOT_ROS_PATH; then
     pip install empy catkin-pkg rospkg defusedxml
     pip install pyqt5
     pip install PySide2
-    pip install bosdyn-client==3.1.0
-    pip install bosdyn-mission==3.1.0
-    pip install bosdyn-api==3.1.0
-    pip install bosdyn-core==3.1.0
-    pip install bosdyn-choreography-client==3.1.0
+    pip install bosdyn-client==3.2.1
+    pip install bosdyn-mission==3.2.1
+    pip install bosdyn-api==3.2.1
+    pip install bosdyn-core==3.2.1
+    pip install bosdyn-choreography-client==3.2.1
     pip install rosdep
     # other necessary packages
     pip install numpy
